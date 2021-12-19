b9da4f290ec538597f6428c0cc50c1e004f5a77d
Aaditya Singhal <aadityasinghal1978@gmail.com>
2021-12-19 13:43:07 +0530

    added mongo model files

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const User = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true},
+    password: {
+        type: String,
+        required: true},
+    email: {
+        type: String,
+        required: true,
+        unique: true}
+    }, {collection: 'user-data'});
+
+const model = mongoose.model('UserData', User);
+
+module.exports = model;
