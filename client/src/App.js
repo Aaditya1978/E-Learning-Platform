7a5594fa545df8081e74fde701fb72bfb96570b3
Aaditya Singhal <aadityasinghal1978@gmail.com>
2022-01-09 15:05:57 +0530

    modified path to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes , Route } from "react-router-dom";
-import Home from "./components/Home";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import UserHome from "./components/UserHome";
-import Navigation from "./components/Navigation";
+import Home from "./components/Home/Home";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import UserHome from "./components/UserHome/UserHome";
+
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          {/* <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
-          <Route exact path="/home" element={<UserHome />} /> */}
-          {/* <Route exact path="/" element={<Navigation/>} /> */}
-           <Route exact path="/" element={<UserHome/>}/>
+          <Route exact path="/home" element={<UserHome />} />
         </Routes>
       </BrowserRouter>
     </div>
