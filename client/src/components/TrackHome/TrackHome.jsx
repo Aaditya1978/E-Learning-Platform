4c051cab7a73bc5ca10efb87bc90c9a6660cf000
Aaditya Singhal <aadityasinghal1978@gmail.com>
2022-01-19 20:24:44 +0530

    added track components

---
+++ client/src/components/TrackHome/TrackHome.jsx
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Card, Button, Row, Col } from "react-bootstrap";
+import { FaArrowRight } from "react-icons/fa";
+import PreLoader from "../PreLoader/PreLoader";
+import UserNavBar from "../UserNavBar/UserNavBar";
+import Footer from "../Footer/Footer";
+import "./TrackHome.css";
+
+export default function TrackHome() {
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const [loading, setLoading] = useState(false);
+  const [track, setTrack] = useState({});
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/login");
+    } else {
+      fetch("http://localhost:5000/api/user/verifyToken", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: localStorage.getItem("token"),
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (!data.user) {
+            localStorage.removeItem("token");
+            navigate("/");
+          }
+        });
+    }
+
+    fetch("http://localhost:5000/api/user/getTrackById", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTrack(data.track);
+        setCourses(data.courses.courses);
+      });
+
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <PreLoader />
+      ) : (
+        <div>
+          {/* Navbar compnent */}
+          <UserNavBar />
+
+          <div className="main-track container">
+            {/* main heading */}
+            <div className="main-heading">
+              <h1>CodeClub - {track.name} Track</h1>
+            </div>
+
+            <div>
+              <Row>
+                {courses.filter((course) => course.difficulty === "Beginner")
+                  .length > 0 ? (
+                  <h2 className="track-heading">Beginner Level Courses</h2>
+                ) : null}
+                {courses.map((course) =>
+                  course.difficulty === "Beginner" ? (
+                    <Col key={course._id}>
+                      <Card className="card-course">
+                        <Card.Img
+                          className="card-img"
+                          variant="top"
+                          src={course.image}
+                        />
+                        <Card.Body>
+                          <Card.Title>{course.name}</Card.Title>
+                          <Card.Text>{course.description}</Card.Text>
+                          <Button variant="primary">
+                            Start Course <FaArrowRight />
+                          </Button>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  ) : (
+                    ""
+                  )
+                )}
+              </Row>
+              <Row>
+                {courses.filter(
+                  (course) => course.difficulty === "Intermediate"
+                ).length > 0 ? (
+                  <h2 className="track-heading">Intermidetate Level Courses</h2>
+                ) : null}
+                {courses.map((course) =>
+                  course.difficulty === "Intermediate" ? (
+                    <Col key={course._id}>
+                      <Card className="card-course">
+                        <Card.Img
+                          className="card-img"
+                          variant="top"
+                          src={course.image}
+                        />
+                        <Card.Body>
+                          <Card.Title>{course.name}</Card.Title>
+                          <Card.Text>{course.description}</Card.Text>
+                          <Button variant="primary">
+                            Start Course <FaArrowRight />
+                          </Button>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  ) : (
+                    ""
+                  )
+                )}
+              </Row>
+              <Row>
+                {courses.filter((course) => course.difficulty === "Advanced")
+                  .length > 0 ? (
+                  <h2 className="track-heading">Advance Level Courses</h2>
+                ) : null}
+                {courses.map((course) =>
+                  course.difficulty === "Advanced" ? (
+                    <Col key={course._id}>
+                      <Card className="card-course">
+                        <Card.Img
+                          className="card-img"
+                          variant="top"
+                          src={course.image}
+                        />
+                        <Card.Body>
+                          <Card.Title>{course.name}</Card.Title>
+                          <Card.Text>{course.description}</Card.Text>
+                          <Button variant="primary">
+                            Start Course <FaArrowRight />
+                          </Button>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  ) : (
+                    ""
+                  )
+                )}
+              </Row>
+            </div>
+          </div>
+
+          {/* footer */}
+          <Footer />
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ client/src/components/Tracks/Tracks.jsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Card, Button, Row, Col } from "react-bootstrap";
+import PreLoader from "../PreLoader/PreLoader";
+import UserNavBar from "../UserNavBar/UserNavBar";
+import Footer from "../Footer/Footer";
+import "./Tracks.css";
+
+export default function Tracks() {
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/login");
+    } else {
+      fetch("api/user/verifyToken", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: localStorage.getItem("token"),
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (!data.user) {
+            localStorage.removeItem("token");
+            navigate("/");
+          }
+        });
+    }
+
+    fetch("api/user/getAllTracks", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTracks(data.tracks);
+      });
+
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <PreLoader />
+      ) : (
+        <div>
+          {/* Navbar compnent */}
+          <UserNavBar />
+
+          <div className="tracks container">
+            {/* main heading */}
+            <div className="main-heading">
+              <h2>Avaliable Tracks</h2>
+            </div>
+
+            {/* tracks cards */}
+            <div className="tracks-card">
+              <Row>
+                {tracks.map((track) => (
+                <Col key={track._id}>
+                  <Card>
+                    <Card.Body>
+                      <Card.Img
+                        fluid="true"
+                        variant="top"
+                        src={track.image}
+                      />
+                      <Card.Title>{track.name}</Card.Title>
+                      <Card.Text>
+                        {track.description}
+                      </Card.Text>
+                      <Button
+                        variant="success"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          navigate(`/track/${track._id}`);
+                        }}
+                      >
+                        Prepare
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                ))}
+              </Row>
+            </div>
+          </div>
+
+          {/* footer */}
+          <Footer />
+        </div>
+      )}
+    </div>
+  );
+}
