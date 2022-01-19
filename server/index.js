de803e949f3fc6905e1fb7aa8ef04d92b0a5a79a
Aaditya Singhal <aadityasinghal1978@gmail.com>
2022-01-19 20:25:36 +0530

    added admin api route

--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const userRoute = require('./routes/user');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const adminRoute = require("./routes/admin");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
-const dbUrl = 'mongodb+srv://codeclub:'+process.env.MONGO_PASSWORD+'@cluster0.m0mam.mongodb.net/codeclub';
+const dbUrl =
+  "mongodb+srv://codeclub:" +
+  process.env.MONGO_PASSWORD +
+  "@cluster0.m0mam.mongodb.net/codeclub";
 
-mongoose.connect(dbUrl, {useNewUrlParser: true});
+mongoose.connect(dbUrl, { useNewUrlParser: true });
 
 app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
 
 app.listen(5000, () => {
-    console.log('Server started on port 5000');
+  console.log("Server started on port 5000");
 });
