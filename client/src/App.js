c211b6eb6e6cfcc132f57c2b0fa0c8c3b27182ec
Aaditya Singhal <aadityasinghal1978@gmail.com>
2022-01-19 20:22:19 +0530

    Added admin and user track routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,15 @@ import Home from "./components/Home/Home";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import UserHome from "./components/UserHome/UserHome";
+import Tracks from "./components/Tracks/Tracks";
+import TrackHome from "./components/TrackHome/TrackHome";
+import Admin from "./components/Admin/Admin";
+import AdminHome from "./components/AdminHome/AdminHome";
+import AddTrack from "./components/AdminHome/AddTrack";
+import EditTrack from "./components/AdminHome/EditTrack";
+import AdminTrack from "./components/AdminHome/AdminTrack"
+import AddCourse from "./components/AdminHome/AddCourse";
+import EditCourse from "./components/AdminHome/EditCourse";
 
 
 function App() {
@@ -11,11 +20,23 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          {/* <Route exact path="/" element={<Home />} />
+
+          {/* User Routes */}
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
-          <Route exact path="/home" element={<UserHome />} /> */}
-          <Route exact path="/" element={<UserHome/>}/>
+          <Route exact path="/home" element={<UserHome />} />
+          <Route exact path="/tracks" element={<Tracks/>}/>
+          <Route exact path="/track/:id" element={<TrackHome/>}/>
+
+          {/* Admin Routes */}
+          <Route exact path="/admin_login" element={<Admin/>}/>
+          <Route exact path="/admin_home" element={<AdminHome/>}/>
+          <Route exact path="/admin_add_track" element={<AddTrack/>}/>
+          <Route exact path="/admin_home/edit/:id" element={<EditTrack/>}/>
+          <Route exact path="/admin_home/view/:id" element={<AdminTrack/>}/>
+          <Route exact path="/admin_add_course/:id" element={<AddCourse/>}/>
+          <Route exact path="/admin_home/edit_course/:courseId" element={<EditCourse/>}/>
         </Routes>
       </BrowserRouter>
     </div>
