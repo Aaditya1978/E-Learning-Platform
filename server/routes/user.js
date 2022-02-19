cb164dd7cd42dd55a0441ab1778b11ddd26ff9b7
Aaditya Singhal <aadityasinghal1978@gmail.com>
2022-02-19 21:55:21 +0530

    added required routes for user

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const axios = require("axios");
 const User = require("../models/user.model");
 const Track = require("../models/track.model");
 const Course = require("../models/course.model");
+const UserCourse = require("../models/user_course.model");
 
 const saltRounds = parseInt(process.env.SALT_ROUNDS);
 
@@ -111,6 +113,36 @@ router.post("/verifyToken", async (req, res) => {
   }
 });
 
+router.post("/getUserData", async (req, res) => {
+  const { token } = req.body;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (user) {
+      const userCourses = await UserCourse.find({
+        user: user._id,
+      });
+      const courses = await Course.find({
+        _id: {
+          $in: userCourses.map((userCourse) => userCourse.course),
+        },
+      });
+      return res.status(200).json({
+        courses: courses,
+        userCourses: userCourses,
+      });
+    } else {
+      return res.status(401).json({
+        error: "User not found",
+      });
+    }
+  } catch (err) {
+    return res.status(401).json({
+      error: "Invalid token",
+    });
+  }
+});
+
 router.get("/getAllTracks", async (req, res) => {
   const tracks = await Track.find({});
   return res.status(200).json({
@@ -119,21 +151,195 @@ router.get("/getAllTracks", async (req, res) => {
 });
 
 router.post("/getTrackById", async (req, res) => {
-  const { id } = req.body;
-  const track = await Track.findById(id);
-  const courses = await Track.findById(id).populate("courses");
-  return res.status(200).json({
-    track: track,
-    courses: courses,
-  });
+  const { id, token } = req.body;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json({
+        error: "User not found",
+      });
+    }
+    const track = await Track.findById(id);
+    const courses = await Track.findById(id).populate("courses");
+    const userCourses = await UserCourse.find({
+      user: user._id,
+    });
+    const userCoursesIds = userCourses.map((course) => course.course);
+    return res.status(200).json({
+      track: track,
+      courses: courses,
+      userCoursesIds: userCoursesIds,
+      userCourses: userCourses,
+    });
+  } catch (err) {
+    return res.status(401).json({
+      error: "Invalid token",
+    });
+  }
+});
+
+router.post("/startCourse", async (req, res) => {
+  const { courseId, token } = req.body;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json({
+        error: "User not found",
+      });
+    }
+    const userCourse = await UserCourse.findOne({
+      user: user._id,
+      course: courseId,
+    });
+    if (userCourse) {
+      return res.status(409).json({
+        error: "User already enrolled in this course",
+      });
+    }
+    const course = await Course.findById(courseId);
+    const length = course.videos.length;
+    const userCourseCreated = await UserCourse.create({
+      user: user._id,
+      course: courseId,
+      checked: Array(length).fill(false),
+      progress: 0,
+      completed: false,
+    });
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(401).json({
+      error: "Invalid token",
+    });
+  }
 });
 
 router.post("/getCourseById", async (req, res) => {
-  const { id } = req.body;
-  const course = await Course.findById(id);
-  return res.status(200).json({
-    course: course,
+  const { id, token } = req.body;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (user) {
+      const course = await Course.findById(id);
+      const userCourse = await UserCourse.findOne({
+        user: user._id,
+        course: id,
+      });
+      return res.status(200).json({
+        course: course,
+        userCourse: userCourse,
+      });
+    } else {
+      return res.status(401).json({
+        error: "User not found",
+      });
+    }
+  } catch (err) {
+    return res.status(401).json({
+      error: "Invalid token",
+    });
+  }
+});
+
+router.post("/handleCourseProgress", async (req, res) => {
+  const { courseId, token, progress, checked } = req.body;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (user) {
+      const userCourse = await UserCourse.findOne({
+        user: user._id,
+        course: courseId,
+      });
+      const course = await Course.findById(courseId);
+      if (userCourse) {
+        userCourse.progress = progress;
+        userCourse.checked = checked;
+        if (course.videos.length === progress) {
+          userCourse.completed = true;
+          userCourse.date_completed = new Date();
+        } else {
+          userCourse.completed = false;
+          userCourse.date_completed = null;
+        }
+        userCourse.save();
+        return res.status(200).json({
+          success: true,
+          userCourse: userCourse,
+        });
+      }
+    } else {
+      return res.status(401).json({
+        error: "User not found",
+      });
+    }
+  } catch (err) {
+    return res.status(401).json({
+      error: "Invalid token",
+    });
+  }
+});
+
+router.get("/contests", async (req, res) => {
+  axios.get("https://kontests.net/api/v1/all").then((response) => {
+    const contests = response.data;
+    return res.status(200).json({
+      contests: contests,
+    });
   });
+});
+
+router.post("/compile", async (req, res) => {
+  const { code, language, input } = req.body;
+  console.log(code, language, input);
+  const compileUrl = "https://judge0-ce.p.rapidapi.com/submissions";
+  await axios
+    .request({
+      method: "POST",
+      url: compileUrl,
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "Content-Type": "application/json",
+        "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+        "x-rapidapi-key": "a2c6754f15msh7fbc3a77f87af08p135901jsn8c82bc01a4a0",
+      },
+      data: {
+        language_id: language,
+        source_code: code,
+        stdin: input,
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      axios
+        .request({
+          method: "GET",
+          url: `compileUrl/${response.data.token}`,
+          params: { base64_encoded: "true", fields: "*" },
+          headers: {
+            "Content-Type": "application/json",
+            "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+            "x-rapidapi-key":
+              "a2c6754f15msh7fbc3a77f87af08p135901jsn8c82bc01a4a0",
+          },
+        })
+        .then((response) => {
+          return res.status(200).json({
+            output: response.data,
+          });
+        })
+        .catch((err) => {
+          return res.status(500).json({
+            error: err.message,
+          });
+        });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        error: err.message,
+      });
+    });
 });
 
 module.exports = router;
